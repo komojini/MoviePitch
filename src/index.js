63b23c8e3896b48094281fc711274d82a7a732f6
qwaezrx <hongjw0309@naver.com>
2023-08-05 11:35:41 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const setupTextArea = document.getElementById('setup-textarea')
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  if (setupTextArea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    movieBossText.innerText = `Ok, just wait a second while AI working...`
+  }
+})

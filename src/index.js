cee8a36caa701bb6993a24c75827215083619e5b
qwaezrx <hongjw0309@naver.com>
2023-08-05 12:21:45 +0900

    index all set

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const setupTextArea = document.getElementById('setup-textarea')
+const setupTextarea = document.getElementById('setup-textarea') 
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
 document.getElementById("send-btn").addEventListener("click", () => {
-  if (setupTextArea.value) {
+  if (setupTextarea.value) {
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, just wait a second while AI working...`
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   }
 })
